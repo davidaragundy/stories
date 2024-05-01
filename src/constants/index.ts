6e3174346609139e5874c56169260a694da2db45
David Aragundy <davidaragundy@outlook.com>
2024-05-01 11:16:45 -0500

    hotfix: fix the base url for forgot password feat (#5)
    * hotfix: fix the base url for forgot password feat
    
    * hotfix: change VERCEL_URL to VERCEL_BRANCH_URL

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,8 @@
 export const EXPIRATION_TIME = 24 * 60 * 60 * 1000;
 
-export const BASE_URL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+export const BASE_URL =
+  process.env.VERCEL_ENV === "production"
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : process.env.VERCEL_ENV === "preview"
+      ? `https://${process.env.VERCEL_BRANCH_URL}`
+      : "http://localhost:3000";
