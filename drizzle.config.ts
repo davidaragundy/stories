1fb0a6dfaba23336cf0bda063b63f7a36d53a9e2
David Aragundy <davidaragundy@outlook.com>
2025-03-14 16:07:50 -0500

    🐛 Map GitHub username

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,4 +10,5 @@ export default defineConfig({
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
+  strict: true,
 });
--- src/shared/lib/auth/setup.ts
+++ src/shared/lib/auth/setup.ts
@@ -90,6 +90,12 @@ export const auth = betterAuth({
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      mapProfileToUser: (profile) => {
+        return {
+          username: profile.login,
+          displayUsername: profile.login,
+        };
+      },
     },
   },
 });
